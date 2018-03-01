6dbd57536803a25bf31174c30b8c7c101a96d9b0
Sebastian Sirb <sebastian.sirb@hpe.com>
2018-03-01 14:33:14 +0200

    added material-ui and some mock json

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  isUserAuthenticated = () => {
+    return false;
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Employee Platform</h1>
+          </header>
+          <p className="App-intro">
+            Built with Java, Spring, React, and Material-UI.
+          </p>
+            {!this.isUserAuthenticated() ? <RaisedButton label="Log In" /> : null}
+        </div>
+    </MuiThemeProvider>
     );
   }
 }
